a9d92f9de34c96c2249b101788834df79b8dc47f
Song <songsiyu13@sjtu.edu.cn>
2017-05-23 14:15:39 +0800

    welcome action

---
+++ src/main/java/com/controlor/Welcome.java
@@ -0,0 +1,17 @@
+package com.controlor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Song on 2017/5/23.
+ */
+@Controller
+public class Welcome {
+
+    @RequestMapping("/")
+    public String welcome()
+    {
+        return "index";
+    }
+}
